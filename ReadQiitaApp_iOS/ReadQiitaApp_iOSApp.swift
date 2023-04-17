c8a24e99282ae9ef459a4f366f15e257d2314f69
MasaharuDobashi <masaharu.dobashi@gmail.com>
2023-04-17 22:40:01 +0900

    Initial Commit

---
+++ ReadQiitaApp_iOS/ReadQiitaApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadQiitaApp_iOSApp.swift
+//  ReadQiitaApp_iOS
+//
+//  Created by 土橋正晴 on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct ReadQiitaApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
